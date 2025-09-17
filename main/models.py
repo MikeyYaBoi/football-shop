baa760c0ddd124f2f0872c603d966f79f5bd6881
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-09-17 08:59:42 +0700

    Tugas 3 complete

--- main/models.py
+++ main/models.py
@@ -1,9 +1,25 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
+    PRODUCT_CHOICES = [
+        ("shoes", "Shoes"),
+        ("ball", "Ball"),
+        ("shin guards", "Shin Guards"),
+        ("jersey", "Jersey"),
+        ("shorts", "Shorts"),
+        ("socks", "Socks"),
+        ("goalkeeper gloves", "Goalkeeper Gloves"),
+
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length=30, choices=PRODUCT_CHOICES,
+                                default="shoes")
     is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
