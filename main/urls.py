b9124a5632a5e8f83d1f6657baa3de54f5fec5b7
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-09-23 16:43:00 +0700

    Added login & register feature

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from main.views import show_main, create_product, show_product, show_json, show_xml, show_json_by_id, show_xml_by_id
-
+from main.views import show_main, create_product, show_product, register, login_user, logout_user
+from main.views import show_json, show_xml, show_json_by_id, show_xml_by_id
 app_name = 'main'
 urlpatterns = [
     path('', show_main, name= 'show_main'),
@@ -10,4 +10,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<uuid:product_id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<uuid:product_id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
+import datetime
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from main.forms import ProductForm
 from main.models import Product
+
+@login_required(login_url='/login')
 def show_main(request):
     product_catalog = Product.objects.all()
     context = {
@@ -10,6 +18,7 @@ def show_main(request):
         "class": "PBP A",
         "npm": "2406496321",
         "product_catalog": product_catalog,
+        "last_login": request.COOKIES.get('last_login', 'Never'),
     }
     return render(request, "main.html", context)
 
@@ -23,6 +32,8 @@ def create_product(request):
     context = {'form': form}
     return render(request, "create_product.html", context)
 
+
+@login_required(login_url='/login')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
     
@@ -57,3 +68,39 @@ def show_json_by_id(request, product_id):
         return HttpResponse(json_data, content_type="application/json")
     except Product.DoesNotExist:
         return HttpResponse(status=404)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+
